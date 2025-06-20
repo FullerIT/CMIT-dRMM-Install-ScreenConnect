cdeaa56c5c9ea8839dfc100ec254b012b1a51ff0
dyerseve <dyerseve@gmail.com>
2025-06-20 14:59:48 -0400

    Update cmit-install-sc.ps1
    fixed UDF url, removed sigcheck

--- cmit-install-sc.ps1
+++ cmit-install-sc.ps1
@@ -1,8 +1,24 @@
-write-host (get-filehash $MyInvocation.MyCommand -Algorithm SHA1).Hash #Writes this script's hash for use in ThreatLocker
+<# cmit-install-sc.ps1
+pellis@cmitsolutions.com
+2025-06-20-001
 
-#connectwise control in UDF :: redux build 2, march 23/seagull
-write-host "ConnectWise Control in UDF"
-write-host "==================================="
+This tries to fix the changes that resulted from the software changes ConnectWise had to apply to Screenconnect on this release: 
+https://docs.connectwise.com/ScreenConnect_Documentation/ScreenConnect_release_notes/ScreenConnect_2025.4_Release_notes
+
+The certificate verification doesn't work with msi files, it would need to be redesigned but I'm not sure why checking that matters, it's a nice to have, but not critical.
+
+dRMM Variables:
+ConnectWiseControlPublicKeyThumbprint [string]
+usrUDF [string]
+ConnectWiseControlBaseUrl [string]
+
+#>
+
+#debug vars
+#$env:ConnectWiseControlPublicKeyThumbprint = "0ba9066856b8518d"
+#$env:usrUDF = "1"
+#$env:ConnectWiseControlBaseUrl = "https://cmitcincy.screenconnect.com"
+#$env:CS_PROFILE_NAME = "CMIT Solutions of Cincinnati & NKY"
 
 #function provided by Datto
 function verifyPackage ($file, $certificate, $thumbprint, $name, $url) {
@@ -41,7 +57,9 @@ function verifyPackage ($file, $certificate, $thumbprint, $name, $url) {
 function CreateJoinLink {
     $null = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\ScreenConnect Client ($env:ConnectWiseControlPublicKeyThumbprint)" -Name ImagePath).ImagePath -Match '(&s=[a-f0-9\-]*)'
     $GUID = $Matches[0] -replace '&s='
-    $apiLaunchUrl= "$($env:ConnectWiseControlBaseUrl)" + "/Host#Access///" + $GUID + "/Join"
+    #$apiLaunchUrl= "$($env:ConnectWiseControlBaseUrl)" + "/Host#Access///" + $GUID + "/" + $GUID + "/Join"
+    #Example: https://cmitcincy.screenconnect.com/Host#Access/All%20Machines/5c00013c-c216-48df-b256-262b63790e7c/5c00013c-c216-48df-b256-262b63790e7c/General
+    $apiLaunchUrl= "$($env:ConnectWiseControlBaseUrl)" + "/Host#Access/All%20Machines/" + $GUID + "/" + $GUID + "/Join"
     New-ItemProperty -Path "HKLM:\Software\CentraStage" -Name "Custom$env:usrUDF" -PropertyType String -Value $apiLaunchUrl -force | out-null
     write-host "- UDF written to UDF#$env:usrUDF."
 }
@@ -53,12 +71,12 @@ if (Test-Path "HKLM:\SYSTEM\CurrentControlSet\Services\ScreenConnect Client ($en
     $company = $env:CS_PROFILE_NAME
     $company = $company.TrimEnd()
     $company = [uri]::EscapeDataString($company)
-    $insturl = $env:ConnectWiseControlBaseUrl + "?e=Access&y=Guest&c=" + $company + "&c=&c=&c=&c=&c=&c="
+    $insturl = $env:ConnectWiseControlBaseUrl + "/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest&c=" + $company + "&c=&c=&c=&c=&c=&c="
     $tmp = "ScreenConnect.ClientSetup.msi"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri $insturl -OutFile $tmp
     #cert from 16/August/2022 to 15/August/2025
-    verifyPackage $tmp "ConnectWise, LLC" "4c2272fba7a7380f55e2a424e9e624aee1c14579" "ConnectWise Control Client Setup" $insturl
+    #verifyPackage $tmp "ConnectWise, LLC" "4c2272fba7a7380f55e2a424e9e624aee1c14579" "ConnectWise Control Client Setup" $insturl
     write-host "- Installing ConnectWise Control..."
     Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList "/i $tmp /qn" -PassThru
     CreateJoinLink
